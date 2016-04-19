411685c0163cd0e642dbfcce0a496fc838be77c9
llaniewski <llaniewski@gmail.com>
2016-04-19 20:28:02 +0200

    Adding tex templates

---
+++ tools/pdf_full.R
@@ -0,0 +1,13 @@
+
+pdf_full = function( ... ) {
+  ret = pdf_document(template = "tools/pdf_small_template.tex", ...)
+  ret$post_processor = function(yaml, input, output,...) {
+    post_doc = pdf_document(
+      pandoc_args = c("--variable",paste("pdffile",output,sep=":")),
+      template = "tools/pdf_full_template.tex"
+    )
+    cat("And now running again ",input," with ",output,"\n")
+    render(input, post_doc)
+  }
+  ret
+}
