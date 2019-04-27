e7751ca1e174fec75688b2f56d44e78558bf74fe
kmarchlewski <kmarchlewski@meil.pw.edu.pl>
2019-04-27 10:58:27 +0000

    Update.

--- code/info2/rk4.h
+++ code/info2/rk4.h
@@ -2,36 +2,36 @@
 #define __RK4_H__
 
 // --------------------------------------------------------------------------
-// Funkcja wykonuje, metod¹ Rungego-Kutty Iv-ego rzêdu,
-// jeden krok ca³kowania skalarnego równania ro¿niczkowego zwyczajnego:
+// Funkcja wykonuje, metoda Rungego-Kutty IV-ego rzedu,
+// jeden krok calkowania skalarnego rownania rozniczkowego zwyczajnego:
 //
 //  dy/dx = fun(x,y),    y(x0)=y0
 //
 // Parametry formalne:
-// x0 - wartoœæ startowa zm. niezale¿nej
-// y0 - wartoœæ startowa zm. zale¿nej
-// h  - krok ca³kowania
-// fun(x,y) - nazwa funkcji obliczaj¹cej prawe strony
-// y1 - obliczona wartoœæ zmiennej zale¿nej w punkcie x0+h
+// x0 - wartosc startowa zm. niezaleznej
+// y0 - wartosc startowa zm. zaleznej
+// h  - krok calkowania
+// fun(x,y) - nazwa funkcji obliczajacej prawe strony
+// y1 - obliczona wartosc zmiennej zaleznej w punkcie x0+h
 
-double rk4( double x0, double y0, double h, double (*fun)(double, double));
+double rk4(double x0, double y0, double h, double (*fun)(double, double));
 
 
 // --------------------------------------------------------------------------
-// Funkcja wykonuje, metod¹ Rungego-Kutty IV-tego rzedu,
-// jeden krok ca³kowania wektorowego równania ro¿niczkowego zwyczjanego:
+// Funkcja wykonuje, metoda Rungego-Kutty IV-tego rzedu,
+// jeden krok calkowania wektorowego rownania rozniczkowego zwyczjanego:
 //
 //  dY/dx = Fun(x,Y),    Y(x0)=Y0
 //
 // Parametry formalne:
-// x0 - wartoœæ startowa zm. niezale¿nej
-// y0 - wartoœæ startowa zm. zale¿nej (tablica n-elementowa)
-// h  - krok ca³kowania
-// n  - liczba równañ
-// fun(x,y,prawastr) - nazwa funkcji obliczaj¹cej prawe strony
-// y1 - obliczona wartoœæ zmiennej zale¿nej w punkcie x0+h
+// x0 - wartosc startowa zm. niezaleznej
+// y0 - wartosc startowa zm. zaleznej (tablica n-elementowa)
+// h  - krok calkowania
+// n  - liczba rownañ
+// fun(x,y,prawastr) - nazwa funkcji obliczajacej prawe strony
+// y1 - obliczona wartosc zmiennej zaleznej w punkcie x0+h
 //      (tablica n-elementowa)
 
-void vrk4( double x0, double y0[], double h, int n, void (*fun)(double, double*, double*), double y1[] );
+void vrk4(double x0, double y0[], double h, int n, void (*fun)(double, double*, double*), double y1[]);
 
 #endif
