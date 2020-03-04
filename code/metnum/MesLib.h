f70efe86e65de1230161d6494112a2021b9343ca
kmarchlewski <kmarchlewski@meil.pw.edu.pl>
2020-03-04 12:30:46 +0100

    Update.

---
+++ code/metnum/MesLib.h
@@ -0,0 +1,182 @@
+//------------------------------- Bardzo pomocne funkcje
+
+int P(int x, int y, int z)
+{
+	return (y*(mx + 1) + x) * 2 + z;
+}
+
+int Q(int x, int y)
+{
+	return y*mx + x;
+}
+
+int DOF(int elidx, int elidy, int locdofid)
+{
+	if (locdofid < 4)
+	{
+		return 2 * (elidy*(mx + 1) + elidx) + locdofid;
+	}
+	else if (locdofid == 4 || locdofid == 5)
+	{
+		return 2 * ((elidy + 1)*(mx + 1) + elidx - 1) + locdofid;
+	}
+	else if (locdofid == 6 || locdofid == 7)
+	{
+		return 2 * ((elidy + 1)*(mx + 1) + elidx - 3) + locdofid;
+	}
+}
+
+const double skala = 1.0;
+
+//------------------------------- MACIERZ SZTYWNOSCI ELEMENTU --------------------------
+const double E = 100.;
+const double nu = 0.3;
+const double elems[] = {
+	0, 1. / 2. - nu / 6., 1. / 8. + nu / 8., -1. / 4. - nu / 12., -1. / 8. + 3.*nu / 8.,
+	-1. / 4. + nu / 12., -1. / 8. - nu / 8., nu / 6., 1. / 8. - 3.*nu / 8.
+};
+const double Md = E / (1 - nu*nu);
+
+const double K[8][8] = { { elems[1], elems[2], elems[3], elems[4], elems[5], elems[6], elems[7], elems[8] },
+{ elems[2], elems[1], elems[8], elems[7], elems[6], elems[5], elems[4], elems[3] },
+{ elems[3], elems[8], elems[1], elems[6], elems[7], elems[4], elems[5], elems[2] },
+{ elems[4], elems[7], elems[6], elems[1], elems[8], elems[3], elems[2], elems[5] },
+{ elems[5], elems[6], elems[7], elems[8], elems[1], elems[2], elems[3], elems[4] },
+{ elems[6], elems[5], elems[4], elems[3], elems[2], elems[1], elems[8], elems[7] },
+{ elems[7], elems[4], elems[5], elems[2], elems[3], elems[8], elems[1], elems[6] },
+{ elems[8], elems[3], elems[2], elems[5], elems[4], elems[7], elems[6], elems[1] } };
+
+//------------------------------- MACIERZ MASOWA ELEMENTU --------------------------
+const double rho = 1;
+const double Mm = rho;
+
+const double M[8][8] = { { 4, 0, 2, 0, 1, 0, 2, 0 },
+{ 0, 4, 0, 2, 0, 1, 0, 2 },
+{ 2, 0, 4, 0, 2, 0, 1, 0 },
+{ 0, 2, 0, 4, 0, 2, 0, 1 },
+{ 1, 0, 2, 0, 4, 0, 2, 0 },
+{ 0, 1, 0, 2, 0, 4, 0, 2 },
+{ 2, 0, 1, 0, 2, 0, 4, 0 },
+{ 0, 2, 0, 1, 0, 2, 0, 4 } };
+
+//------------------------------- ELIMINACJA GAUSSA -------------------------
+void Gauss(int n, double **M, double *f, double *x)
+{
+	int i, j, k;
+	double s;
+	double wsp;
+
+	double **a;
+	double *b;
+
+	b = new double[n];
+	a = new double*[n];
+	for (int i = 0; i < n; ++i)
+		a[i] = new double[n];
+
+	for (int i = 0; i < n; ++i)
+	{
+		for (int j = 0; j < n; ++j)
+		{
+			a[i][j] = M[i][j];
+		}
+		b[i] = f[i];
+	}
+
+	//eliminacja
+
+	// wiersz przy pomocy którego eliminujemy elementy
+	for (i = 0; i < n - 1; i++)
+	{
+		// wersz którego elementy eliminujemy
+		for (k = i + 1; k < n; k++)
+		{
+			wsp = a[k][i] / a[i][i];
+			// odejmowanie elementów wiersza "i" od wiersza "k"
+			for (j = i + 1; j < n; j++)
+				a[k][j] -= a[i][j] * wsp;
+
+			// odejmowanie prawych stron
+			b[k] -= b[i] * wsp;
+		}
+	}
+
+	//rozwiazanie ukladu
+	x[n - 1] = b[n - 1] / a[n - 1][n - 1];
+
+	for (i = n - 1; i >= 0; i--)
+	{
+		s = 0.0;
+		for (j = i + 1; j < n; j++)
+			s += a[i][j] * x[j];
+
+		x[i] = (b[i] - s) / a[i][i];
+	}
+
+	for (int i = 0; i < n; ++i)
+		delete[] a[i];
+
+	delete[] a;
+	delete[] b;
+}
+
+void quad(double * p, int x, int y){
+	double px[4], py[4];
+	px[0] = skala*p[P(x, y, 0)] + x;
+	py[0] = skala*p[P(x, y, 1)] + y;
+	px[1] = skala*p[P(x + 1, y, 0)] + x + 1;
+	py[1] = skala*p[P(x + 1, y, 1)] + y;
+	px[2] = skala*p[P(x + 1, y + 1, 0)] + x + 1;
+	py[2] = skala*p[P(x + 1, y + 1, 1)] + y + 1;
+	px[3] = skala*p[P(x, y + 1, 0)] + x;
+	py[3] = skala*p[P(x, y + 1, 1)] + y + 1;
+	setcolor(0.15);
+	polygon(px, py, 4);
+	setgray(1);
+	line(px[0], py[0], px[1], py[1]);
+	line(px[1], py[1], px[2], py[2]);
+	line(px[2], py[2], px[3], py[3]);
+	line(px[3], py[3], px[0], py[0]);
+}
+
+//------------------------------- Rysowanie strzalki
+void strzalka(double px, double py, double vx, double vy, double sk, double k) {
+	vx *= sk;
+	vy *= sk;
+	setcolor(1);
+	line(px, py, px + vx, py + vy);
+	px += vx;
+	py += vy;
+	double d = sqrt(vx*vx + vy*vy);
+	vx *= k; // d;
+	vy *= k; // d;
+	line(px, py, px - vx - vy / 2, py - vy + vx / 2);
+	line(px, py, px - vx + vy / 2, py - vy - vx / 2);
+}
+
+//------------------------------- Rysowanie calego ukladu
+void draw(double * p, double * f) {
+	double px, py, vx, vy;
+	for (int x = 0; x<mx; x++){
+		for (int y = 0; y<my; y++){
+			quad(p, x, y);
+		}
+	}
+	for (int x = 0; x<mx + 1; x++){
+		for (int y = 0; y<my + 1; y++){
+			if (fix[P(x, y, 1)]) {
+				line(x, y, x + 0.2, y - 0.4);
+				line(x, y, x - 0.2, y - 0.4);
+				line(x + 0.2, y - 0.4, x - 0.2, y - 0.4);
+			}
+			if (fix[P(x, y, 0)]) {
+				line(x, y, x - 0.4, y + 0.2);
+				line(x, y, x - 0.4, y - 0.2);
+				line(x - 0.4, y - 0.2, x - 0.4, y + 0.2);
+			}
+			if ((f[P(x, y, 0)] != 0) || (f[P(x, y, 1)] != 0)) {
+				strzalka(skala*p[P(x, y, 0)] + x, skala*p[P(x, y, 1)] + y, f[P(x, y, 0)], f[P(x, y, 1)], 25, 0.3);
+			}
+		}
+	}
+}
---
+++ code/metnum/winbgi2.h
@@ -0,0 +1,341 @@
+//
+// graphics.h  -- One of the files required to run BGI graphics programs
+//
+// You don't need to edit this file, or print it out.
+
+#ifndef __GRAPHICS2_H__
+#define __GRAPHICS2_H__
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#define far
+#define huge
+#define random(range) (rand() % (range))
+
+enum colors { 
+    BLACK, BLUE, GREEN, CYAN, RED, MAGENT, BROWN, LIGHTGRAY, DARKGRAY, 
+    LIGHTBLUE, LIGHTGREEN, LIGHTCYAN, LIGHTRED, LIGHTMAGENTA, YELLOW, WHITE
+};
+
+enum write_modes { 
+    COPY_PUT, XOR_PUT, OR_PUT, AND_PUT, NOT_PUT 
+};
+enum line_styles { 
+    SOLID_LINE, DOTTED_LINE, CENTER_LINE, DASHED_LINE, USERBIT_LINE
+};
+enum fill_styles { 
+    EMPTY_FILL, SOLID_FILL, LINE_FILL, LTSLASH_FILL, SLASH_FILL, BKSLASH_FILL,
+    LTBKSLASH_FILL, HATCH_FILL, XHATCH_FILL, INTERLEAVE_FILL, WIDE_DOT_FILL, 
+    CLOSE_DOT_FILL, USER_FILL
+};
+enum text_directions { 
+    HORIZ_DIR, VERT_DIR
+};
+enum font_types { 
+    DEFAULT_FONT, TRIPLEX_FONT, SMALL_FONT, SANSSERIF_FONT, GOTHIC_FONT 
+};
+
+#define LEFT_TEXT					0
+#define CENTER_TEXT					1
+#define RIGHT_TEXT					2
+#define BOTTOM_TEXT					0
+#define TOP_TEXT					2
+
+#define NORM_WIDTH					1
+#define THICK_WIDTH					3
+
+#define DOTTEDLINE_LENGTH				2
+#define CENTRELINE_LENGTH				4
+
+#define USER_CHAR_SIZE					0
+#define MAXCOLORS					63 
+
+#define CLIP_ON						1
+#define CLIP_OFF					0
+
+#define TOP_ON						1
+#define TOP_OFF					        0
+
+#define LEFT_BUTTON						0
+#define RIGHT_BUTTON			        1
+
+enum graphics_errors {
+	grOk = 0,
+	grNoInitGraph = -1,
+	grNotDetected = -2,
+	grFileNotFound = -3,
+	grInvalidDriver	= -4,
+	grNoLoadMem = -5,
+	grNoScanMem = -6,
+	grNoFloodMem = -7,
+	grFontNotFound = -8,
+	grNoFontMem = -9,
+	grInvalidMode =	-10,
+	grError = -11,
+	grIOerror = -12,
+	grInvalidFont = -13,
+	grInvalidFontNum = -14,
+	grInvalidDeviceNum = -15,
+	grInvalidVersion = -18
+};
+
+
+/* Graphics drivers constants, includes X11 which is particular to XBGI. */
+#define DETECT						0
+#define CGA						1
+#define MCGA						2
+#define EGA						3
+#define EGA64						4
+#define EGAMONO						5
+#define IBM8514						6
+#define HERCMONO					7
+#define ATT400						8
+#define VGA						9
+#define PC3270						10
+
+/* Graphics modes constants. */
+#define CGAC0						0
+#define CGAC1						1
+#define CGAC2						2
+#define CGAC3						3
+#define CGAHI						4
+
+#define MCGAC0						0
+#define MCGAC1						1
+#define MCGAC2						2
+#define MCGAC3						3
+#define MCGAMED						4
+#define MCGAHI						5
+
+#define EGALO						0
+#define EGAHI						1
+
+#define EGA64LO						0
+#define EGA64HI						1
+
+#define EGAMONOHI					3
+
+#define HERCMONOHI					0
+
+#define ATT400C0					0
+#define ATT400C1					1
+#define ATT400C2					2
+#define ATT400C3					3
+#define ATT400MED					4
+#define ATT400HI					5
+
+#define VGALO						0
+#define VGAMED						1
+#define VGAHI						2
+#define VGAMAX                                          3
+
+#define PC3270HI					0
+
+#define IBM8514LO					0
+#define IBM8514HI					1
+
+typedef struct arccoordstype {
+    int x;
+    int y;
+    int xstart;
+    int ystart;
+    int xend;
+    int yend;
+} arccoordstype;
+
+typedef char fillpatterntype[8];
+
+typedef struct fillsettingstype {
+    int pattern;
+    int color;
+} fillsettingstype;
+
+typedef struct linesettingstype {
+    int linestyle;
+    unsigned int upattern;
+    int thickness;
+} linesettingstype;
+
+typedef struct palettetype {
+    unsigned char size;
+    signed char colors[64];
+} palettetype;
+
+typedef struct textsettingstype {
+    int font;	
+    int direction;  
+    int charsize;
+    int horiz;
+    int vert;
+} textsettingstype;
+
+typedef struct viewporttype {
+    int left;
+    int top;
+    int right;
+    int bottom;
+    int clip;
+} viewporttype;
+
+
+#ifndef NOT_USE_PROTOTYPES
+#define PROTO(ARGS) ARGS
+#else
+#define PROTO(ARGS) ()
+#endif
+
+#if defined(__cplusplus) 
+extern "C" {
+#endif
+
+//
+// Setting this variable to 0 increase speed of drawing but
+// correct redraw is not possible. By default this variable is initialized by 1
+//
+extern int bgiemu_handle_redraw;
+
+//
+// Default mode choosed by WinBGI if DETECT value is specified for 
+// device parameter of initgraoh(). Default value is VGAMAX which
+// cause creation of maximized window (resolution depends on display mode)
+//
+extern int bgiemu_default_mode;
+
+void _graphfreemem PROTO((void *ptr, unsigned int size));
+void* _graphgetmem PROTO((unsigned int size));
+void arc PROTO((int, int, int, int, int));
+void bar PROTO((int, int, int, int));
+void bar3d PROTO((int, int, int, int, int, int));
+void circle PROTO((double, double, int));
+void circle_s PROTO((double, double, double, double));
+void cleardevice PROTO((void));
+void clearviewport PROTO((void));
+void closegraph PROTO((void));
+void detectgraph PROTO((int *, int *));
+void drawpoly PROTO((int, int *));
+void ellipse PROTO((int, int, int, int, int, int));
+void fillellipse PROTO((int, int, int, int));
+void fillpoly PROTO((int, int *));
+void floodfill PROTO((int, int, int));
+void getarccoords PROTO((arccoordstype *));
+void getaspectratio PROTO((int *, int *));
+int getbkcolor PROTO((void));
+int getcolor PROTO((void));
+palettetype* getdefaultpalette PROTO((void));
+char* getdrivername PROTO((void));
+void getfillpattern PROTO((char const *));
+void getfillsettings PROTO((fillsettingstype *));
+int getgraphmode PROTO((void));
+void getimage PROTO((int, int, int, int, void *));
+void getlinesettings PROTO((linesettingstype *));
+int getmaxcolor PROTO((void));
+int getmaxmode PROTO((void));
+int getmaxx PROTO((void));
+int getmaxy PROTO((void));
+char* getmodename PROTO((int));
+void getmoderange PROTO((int, int *, int *));
+void getpalette PROTO((palettetype *));
+int getpalettesize PROTO((void));
+unsigned int getpixel PROTO((int, int));
+void gettextsettings PROTO((textsettingstype *));
+void getviewsettings PROTO((viewporttype *));
+int getx PROTO((void));
+int gety PROTO((void));
+void graphdefaults PROTO((void));
+char* grapherrormsg PROTO((int));
+int graphresult PROTO((void));
+unsigned int imagesize PROTO((int, int, int, int));
+void initgraph PROTO((int *, int *, char const *,int , int));
+int installuserdriver PROTO((char const *, int *));
+int installuserfont PROTO((char const *));
+void line PROTO((double, double, double, double));
+void linerel PROTO((int, int));
+void lineto_old PROTO((int, int));
+void moverel PROTO((int, int));
+void moveto PROTO((int, int));
+void outtext PROTO((char const *));
+void outtextxy PROTO((int, int, char const *));
+void pieslice PROTO((int, int, int, int, int));
+void putimage PROTO((int, int, void *, int));
+void putpixel PROTO((int, int, int));
+void point PROTO((double, double));
+void rectangle PROTO((int, int, int, int));
+int registerbgidriver PROTO((void *));
+int registerbgifont PROTO((void *));
+void restorecrtmode PROTO((void));
+void sector PROTO((int, int, int, int, int, int));
+void setactivepage PROTO((int));
+void setallpalette PROTO((palettetype *));
+void setaspectratio PROTO((int, int));
+void setbkcolor PROTO((int));
+void setcolor PROTO((double));
+void setgray PROTO((double));
+void setfillpattern PROTO((char const *, int));
+void setfillstyle PROTO((int, int));
+unsigned int setgraphbufsize PROTO((unsigned int));
+void setgraphmode PROTO((int));
+void setlinestyle PROTO((int, unsigned int, int));
+void setpalette PROTO((int, int));
+void setrgbpalette PROTO((int, int, int, int));
+void settextjustify PROTO((int, int));
+void settextstyle PROTO((int, int, int));
+void setusercharsize PROTO((int, int, int, int));
+void setviewport PROTO((int, int, int, int, int));
+void setvisualpage PROTO((int));
+void setwritemode PROTO((int));
+int textheight PROTO((char const *));
+int textwidth PROTO((char const *));
+int getch PROTO((void));
+int kbhit PROTO((void));		
+void delay PROTO((unsigned msec));
+void restorecrtmode PROTO((void));
+
+bool mouseup();
+bool mousedown();
+void clearmouse();
+int mouseclickx();
+int mouseclicky();
+int mousecurrentx();
+int mousecurrenty();
+int whichmousebutton();
+
+int getkey();
+
+/* ---------------- DODATKI --------------- */
+inline void graphics(int sx, int sy)
+{
+	int GraphDriver=0,GraphMode=0;
+	initgraph( &GraphDriver, &GraphMode, "", sx, sy ); // otwarcie okna grafiki
+	setviewport(0, 0, sx, sy, 0);                      // zmiana obszaru rysowania
+}
+
+inline int clear() {
+	clearviewport();
+	return 0;
+}
+
+void startdelay(unsigned msec);
+void stopdelay();
+int animate(int fps);
+inline int wait() {
+	while (animate(-1));
+	return 0;
+}
+
+void scale (double, double, double, double);
+void log_scale (double, double, double, double);
+void title PROTO((char *, char *, char *));
+void chart PROTO((double * x, double * y, int n, char * xlab, char * ylab, char * main_title));
+
+void lineto(double x0, double y0);
+void reset();
+void polygon(double x[], double y[], int n);
+
+
+/* ---------------------------------------- */
+
+#if defined(__cplusplus) 
+};
+#endif
+
+#endif
