c8289c711a567f1d717be8ba988fdb491618a358
mrutkowski-aero <mariusz.rutkowski@pw.edu.pl>
2024-04-15 10:59:10 +0200

    Update gauss.h
    Added translation

--- code/info2/gauss.h
+++ code/info2/gauss.h
@@ -13,7 +13,17 @@ Argumenty:
   x - wskaznik na tablice przechowujaca rozwiazanie ukladu rownan,
   b - wskaznik na tablice prawych stron ukladu rownan.
 */
+/*
+Function solves a system of linear equations
+A * x = b
+using the Gauss elimination method.
 
+Functions arguments:
+  N - number of equations,
+  A - pointer to two-dimensional array,
+  x - pointer to an array storing solution of the system of equations,
+  b - pointer to an array storing the right hand side vector b.
+*/
 void gauss(int N, double **A, double *x, double *b);
 
 #endif
