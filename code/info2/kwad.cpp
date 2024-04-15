c5b3d59ea97efe0f3ef1cd4ef7ae38e88d88a47d
mrutkowski-aero <mariusz.rutkowski@pw.edu.pl>
2024-04-15 11:01:54 +0200

    Translation and corrected h calculation

--- code/info2/kwad.cpp
+++ code/info2/kwad.cpp
@@ -1,13 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
 #include <math.h>
 
 // oblicza calke metoda trapezow
+// computes definite integral using complex the trapezoidal rule 
 double trapez(double a, double b, double (*pf)(double), int n )
 {
-  double h    = (b - a) / (n - 1);
+  double h    = (b - a) / n;
   double suma = 0.5 * (pf(a) + pf(b));
   double x    = a;
 
-  for (int i = 1; i < n-1; i++)
+  for (int i = 0; i < n-1; i++)
   {
     x    += h;
     suma += pf(x);
@@ -15,17 +18,18 @@ double trapez(double a, double b, double (*pf)(double), int n )
   return suma * h;
 }
 
-
 // oblicza calke metoda simpsona
+// computes definite integral using complex the Simpson rule 
 double simpson( double a, double b, double (*pf)(double), int n )
 {
   double  x    = a;
-  int     nc   = abs((n - 2) / 2 + 1) * 2 + 1;  // poprawione n tak by bylo nieparzyste i >= n_old
-  double  h    = (b - a) / (nc - 1);
+  double  h    = (b - a) / (2*n);
   double  h2   = h * 2;
-  double  suma = pf(a) + 4. *pf(a + h) + pf(b);
+  double  x1   = a + h;
 
-  for (int i = 3; i < nc; i += 2)
+  double  suma = pf(a) + 4. *pf(x1) + pf(b);
+
+  for (int i = 0; i < n-1; i += 1)
   {
     x    += h2;
     suma += 2. * pf(x) + 4. * pf(x + h);
