998ced16b79cf6350d12883c14b76a0f71bea5e5
mrutkowski-aero <mariusz.rutkowski@pw.edu.pl>
2024-04-15 11:02:24 +0200

    Translation and corrected h calculation

--- code/info2/kwad.h
+++ code/info2/kwad.h
@@ -1,15 +1,25 @@
 #ifndef __KWAD_H__
 #define __KWAD_H__
 
+
 //////////////////////////////////////////////////////////////////
 // oblicza metoda trapezow calke funkcji pf w przedziale <a,b> 
 // w oparciu o n punktow (n >= 2)
+
+// computes definite integral on the range <a,b> with complex
+// trapezoidal rule on (n >= 1) subdomains
+// (*pf)(double) is a pointer to a function with one argument
 //////////////////////////////////////////////////////////////////
 double trapez( double a, double b, double (*pf)(double), int n);
+
 
 //////////////////////////////////////////////////////////////////
 // oblicza metoda simpsona calke funkcji pf w przedziale <a,b> 
 // w oparciu o n punktow (n >= 2)
+
+// computes definite integral on the range <a,b> with complex
+// Simpson rule on (n >= 1) subdomains
+// (*pf)(double) is a pointer to a function with one argument
 //////////////////////////////////////////////////////////////////
 double simpson( double a, double b, double (*pf)(double), int n);
 
