b65a805bc71df5bab610f554126eb2a5513a65de
Lukasz Laniewski-Wollk <lukasz.laniewski@pw.edu.pl>
2025-08-13 14:10:48 +0200

    Correcting pdf generation for using right paths

--- tools/pdf_full.R
+++ tools/pdf_full.R
@@ -1,10 +1,15 @@
 
 pdf_full = function( ... ) {
-  ret = pdf_document(template = "tools/pdf_small_template.tex", ...)
+  pandoc_args = c("--resource-path",paste(".",TOOLS,sep=":"))
+  ret = pdf_document(
+    template = "pdf_small_template.tex",
+    pandoc_args = pandoc_args,
+    ...
+  )
   ret$post_processor = function(yaml, input, output,...) {
     post_doc = pdf_document(
-      pandoc_args = c("--variable",paste("pdffile",output,sep=":")),
-      template = "tools/pdf_full_template.tex"
+      pandoc_args = c("--variable",paste("pdffile",output,sep=":"), pandoc_args),
+      template = "pdf_full_template.tex"
     )
     cat("And now running again ",input," with ",output,"\n")
     ret = render(input, post_doc)
